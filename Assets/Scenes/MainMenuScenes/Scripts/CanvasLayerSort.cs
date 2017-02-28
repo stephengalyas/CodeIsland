b07a0f74cf732b6471aae3cdcf11a913b1ad1a25
Michael Donofrio <michael.a.donofrio@hotmail.com>
2017-02-27 23:20:31 -0500

    First Build

---
+++ Assets/Scenes/MainMenuScenes/Scripts/CanvasLayerSort.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasLayerSort: MonoBehaviour {
+
+
+	public string LayerName = "LayerName";
+
+	public void Start(){
+
+		GetComponent<Canvas>().sortingLayerName = LayerName;
+	}
+
+
+}
---
+++ Assets/Scenes/MainMenuScenes/Scripts/ParticleLayerSort.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleLayerSort : MonoBehaviour {
+	
+
+	public string LayerName = "LayerName";
+
+	public void Start(){
+
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = LayerName;
+	}
+
+
+}
